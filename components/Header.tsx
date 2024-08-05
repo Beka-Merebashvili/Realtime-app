dc4cae57375ee480293ca9f79baa170679694b8e
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-08-05 18:48:18 +0400

    add types

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link href="/" className="md:flex-1">
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+    </div>
+  );
+}
+
+export default Header;
