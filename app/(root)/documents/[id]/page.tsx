2a26b89b673875de6b0cf22c09b3337a19c27595
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-08-06 12:30:37 +0400

    add sing in & sing up func

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Document() {
@@ -7,6 +8,12 @@ function Document() {
     <div>
       <Header>
         <p className='text-white'>test</p>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
       </Header>
       <Editor />
     </div>
