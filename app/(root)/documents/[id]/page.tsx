abdd9a4ef8bc2716e79cabab1fc0f101c9d1fce0
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-08-07 11:50:35 +0400

    add collaaborativeroom component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,21 +1,9 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import React from 'react'
+
 
 function Document() {
   return (
     <div>
-      <Header>
-        <p className='text-white'>test</p>
-        <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-      </Header>
-      <Editor />
+      
     </div>
   )
 }
