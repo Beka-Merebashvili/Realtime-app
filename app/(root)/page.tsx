574a7cfd2d0bab98e9827989b17609e6f0ce6201
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-08-10 19:32:03 +0400

    add empty document page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,16 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn";
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Image from 'next/image';
 
 const Home = async () => {
   const clerkUser = await currentUser();
   if(!clerkUser) redirect('/sign-in');
+
+  const documents = [];
 
   return (
     <main className="home-container">
@@ -18,6 +22,27 @@ const Home = async () => {
           </SignedIn>
         </div>
     </Header>
+
+    {documents.length > 0 ?(
+      <div></div>
+    ) : (
+      <div className="document-list-empty">
+        <Image 
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn 
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+      </div>
+    ) }
+
+
     </main>
   );
 };
--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+'use client'
 
-const AddDocumentBtn = () => {
+import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+
+const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
+
+    const addDocumentHandler = async () => {
+        
+    }
+
   return (
-    <div>AddDocumentBtn</div>
+    <Button type="submit" onClick={addDocumentHandler} className="gradient-blue flex gap-1 shadow-md">
+    <Image 
+      src="/assets/icons/add.svg" alt="add" width={24} height={24}
+    />
+    <p className="hidden sm:block">Start a blank document</p>
+  </Button>
   )
 }
 
