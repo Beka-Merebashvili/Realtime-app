ae1c6753c6996e528a9c1323ce005865e62418ab
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-08-21 12:09:24 +0400

    add removeCollaborator action

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -110,3 +110,24 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
       console.log(`Error happened while updating a room access: ${error}`);
     }
   }
+
+  export const removeCollaborator = async ({ roomId, email }: {roomId: string, email: string}) => {
+    try {
+      const room = await liveblocks.getRoom(roomId)
+  
+      if(room.metadata.email === email) {
+        throw new Error('You cannot remove yourself from the document');
+      }
+  
+      const updatedRoom = await liveblocks.updateRoom(roomId, {
+        usersAccesses: {
+          [email]: null
+        }
+      })
+  
+      revalidatePath(`/documents/${roomId}`);
+      return parseStringify(updatedRoom);
+    } catch (error) {
+      console.log(`Error happened while removing a collaborator: ${error}`);
+    }
+  }
